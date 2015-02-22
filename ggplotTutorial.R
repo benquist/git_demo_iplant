fff920a54d1e40ba6b31728177690381cec9e1b7
Brian J. Enquist <benquist@email.arizona.edu>
2015-02-22 14:45:18 -0700

    box plots with ggplot2

--- ggplotTutorial.R
+++ ggplotTutorial.R
@@ -1,2 +1,49 @@
-#ggplot learning
-# Brian J. Enquist 
+########################
+# ggplot learning
+# Brian J. Enquist
+# February 22 2015
+#######################
+
+# this is the script way of installing packages
+install.packages ("ggplot2", dependencies = TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+#load libraries
+library(ggplot2)
+library(reshape2)
+library(ggthemes)
+library(plyr)
+
+#what are the data and how do we map them
+myplot <- ggplot(data=iris, aes(x = Sepal.Length, y = Sepal.Width))
+summary(myplot)
+
+myplot + geom_point() 
+myiris <- iris
+
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color=Species)) +
+  geom_point(aes(shape = Species), size =3)
+
+## Exercise 1
+d2 <- diamonds[sample(1:dim(diamonds)[1],1000),]
+dim(diamonds)
+dim(diamonds)[1]
+
+head(d2)
+
+myplot2 <- ggplot(data=d2, aes(x = carat, y = price))
+summary(myplot2)
+myplot2 + geom_point(aes(color = color), size =6) 
+#myplot2 + geom_point(aes(shape = color), size =3) 
+
+#another way
+ggplot(d2, aes(carat, price, color=color)) + geom_point()
+
+#how to visualize them
+
+head(iris)
+ddply(iris, .(Species), summarize)
+df <- melt (iris, id)
+dcast(df, Species ~ variable, mean)
